ac6df537cf53394d3e1a4589d664398f3eb8f35c
giuseppe <giuseppe.foza@futuroprevidencia.com.br>
2025-01-17 13:36:17 -0300

    First Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { JwtModule } from './jwt/modules/jwt.module';
 import { DatabaseModule } from './database/modules/database.module';
 import { RedisModule } from './redis/modules/redis.module';
-import { JwtMiddleware } from './jwt/presentation/middlewares/jwt.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from '@/features/product/modules/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    JwtModule,
     DatabaseModule,
     RedisModule,
     ProductModule,
@@ -18,8 +15,4 @@ import { ProductModule } from '@/features/product/modules/product.module';
   controllers: [AppController],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(JwtMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
